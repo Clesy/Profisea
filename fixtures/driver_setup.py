5ab9389ec6b7ac22aa97be6f54dce385e002015f
Evgeniy <gerasum04@gmail.com>
2023-02-11 03:15:26 +0200

    Add: requirements.txt Delete: webdriver-manager log file

--- fixtures/driver_setup.py
+++ fixtures/driver_setup.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
@@ -6,7 +8,9 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture
 def firefox_driver_setup():
-    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=firefox_options())
+    browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(),
+                                service_log_path=os.devnull,
+                                options=firefox_options())
     browser.implicitly_wait(10)
     browser.fullscreen_window()
     yield browser
